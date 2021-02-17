cf3f90724e5f5e4941759841718583422030e480
sranjan001 <sranjan@pivotal.io>
2021-02-17 11:03:48 +0800

    Project setup

---
+++ src/main/java/com/vmware/training/spring301/Spring301Application.java
@@ -0,0 +1,13 @@
+package com.vmware.training.spring301;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring301Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring301Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/vmware/training/spring301/Spring301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.training.spring301;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring301ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

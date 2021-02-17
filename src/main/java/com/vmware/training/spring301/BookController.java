cb7d5b44b2533ce9f8ecd4411406177526eb955a
sranjan001 <sranjan@pivotal.io>
2021-02-17 11:37:21 +0800

    200 Test. Introduce /books endpoint

--- src/main/java/com/vmware/training/spring301/BookController.java
+++ src/main/java/com/vmware/training/spring301/BookController.java
@@ -1,8 +1,13 @@
 package com.vmware.training.spring301;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BookController {
 
+    @GetMapping("/books")
+    public void getBooks() {
+
+    }
 }
--- src/test/java/com/vmware/training/spring301/BookControllerTests.java
+++ src/test/java/com/vmware/training/spring301/BookControllerTests.java
@@ -1,24 +1,36 @@
 package com.vmware.training.spring301;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(BookControllerTests.class)
+@WebMvcTest(BookController.class)
 public class BookControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
 
 
+    /**
+     * This test can be disabled or deleted. Shows the importance of updating the test cases as the code is developed
+     * over time
+     */
     @Test
+    @Disabled
     public void getBooks_WillReturn_404() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/books"))
+        mockMvc.perform(get("/books"))
                 .andExpect(status().is(404));
+    }
+
+    @Test
+    public void getBooks_WillReturn_200() throws Exception {
+        mockMvc.perform(get("/books"))
+                .andExpect(status().is(200));
     }
 
 }
